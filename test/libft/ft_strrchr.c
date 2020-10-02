c0cd1b43614b637b64170f0364398bc27cfcaccb
subin195-09 <k_subin@naver.com>
2020-10-02 16:21:36 +0900

    10/02

--- test/libft/ft_strrchr.c
+++ test/libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 17:22:24 by skim              #+#    #+#             */
-/*   Updated: 2020/09/30 17:22:25 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/02 16:21:15 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 char	*ft_strrchr(const char *str, int c)
 {
-	int	i;
+	char	*str_p;
+	int		i;
 
+	str_p = (char *)str;
 	i = ft_strlen(str) - 1;
 	while (i >= 0)
 	{
-		if (str[i] == (unsigned char)c)
-			return ((char *)&str[i]);
+		if (str_p[i] == (char)c)
+			return (&str_p[i]);
 		i--;
 	}
 	return (0);
