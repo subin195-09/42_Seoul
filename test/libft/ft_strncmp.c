3b51febf07f3f8daa282410cb81529f2bf662a2b
subin195-09 <k_subin@naver.com>
2020-10-02 16:25:44 +0900

    10/02

--- test/libft/ft_strncmp.c
+++ test/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 16:47:27 by skim              #+#    #+#             */
-/*   Updated: 2020/10/02 14:42:09 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/02 16:25:26 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int		ft_strncmp(const char *s1, const char *s2, int n)
 {
-	char *s1_p;
-	char *s2_p;
+	unsigned char *s1_p;
+	unsigned char *s2_p;
 	int i;
 
 	i = 0;
-	s1_p = (char *)s1;
-	s2_p = (char *)s2;
+	s1_p = (unsigned char *)s1;
+	s2_p = (unsigned char *)s2;
 	while (i < n && s1_p[i] && s2_p[i])
 	{
 		if (s1_p[i] != s2_p[i])
