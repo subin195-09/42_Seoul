b4333167fed63f0bd45d8aec67a0164e3895aaf6
subin195-09 <k_subin@naver.com>
2020-10-03 00:39:26 +0900

    10/03

--- test/libft/ft_putnbr_fd.c
+++ test/libft/ft_putnbr_fd.c
@@ -6,14 +6,25 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 15:07:04 by skim              #+#    #+#             */
-/*   Updated: 2020/10/02 16:46:47 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 00:39:07 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void 	write_num(long long temp, int fd)
+{
+	if (temp == 0)
+		return ;
+	else
+	{
+		write_num(temp / 10, fd);
+		char temp_write = (temp % 10) + '0';
+		write(fd, &temp_write, 1);
+	}
+}
 
-void	ft_putnbr_fd(int n, int fd)
+void			ft_putnbr_fd(int n, int fd)
 {
 	long long	temp;
 
@@ -26,12 +37,5 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	if (n < 0)
 		write(1, "-", 1);
-
-	while (temp > 0)
-	{
-		char temp_write = (temp % 10) + '0';
-		write(fd, &temp_write, 1);
-		temp /= 10;
-	}
-
+	write_num(temp, fd);
 }
