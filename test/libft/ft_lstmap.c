c4ff3ade8b757ef4c0aac86fa87fe1b36cc0caad
subin195-09 <k_subin@naver.com>
2020-10-03 01:28:44 +0900

    10/03

--- test/libft/ft_lstmap.c
+++ test/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 18:10:06 by skim              #+#    #+#             */
-/*   Updated: 2020/10/03 01:21:15 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 01:28:33 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!lst || !f || !del)
 		return (0);
-	if (!(head = ft_lstnew((*f)(lst->content))))
+	if (!(head = ft_lstnew(f(lst->content))))
 		return (0);
 	while(lst->next)
 	{
 		lst = lst->next;
-		if (!(temp_lst = ft_lstnew((*f)(lst->content))))
+		if (!(temp_lst = ft_lstnew(f(lst->content))))
 		{
 			ft_lstclear(&head, del);
 			return (0);
