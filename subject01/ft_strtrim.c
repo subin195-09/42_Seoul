2c7b0054473d5bf76a00abfce28803ff8d7d9f0c
subin195-09 <k_subin@naver.com>
2020-10-03 16:48:20 +0900

    10/03

--- subject01/ft_strtrim.c
+++ subject01/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 17:26:49 by skim              #+#    #+#             */
-/*   Updated: 2020/10/03 15:22:54 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 16:48:11 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,16 @@ char	*ft_strtrim(const char *s1, const char *set)
 	int		start;
 	int		end;
 
-	if (!set && !s1)
-		return (ft_strdup(""));
+	if (!set || !s1)
+		return (0);
 	start = 0;
 	while (ft_strchr(set, s1[start]))
 		start++;
-	end = ft_strlen(s1) - 1;
+	end = ft_strlen(s1);
 	while (ft_strchr(set, s1[end]))
 		end--;
 	if (start >= end)
 		return (ft_strdup(""));
-	if (!(result = malloc(end - start + 2)))
-		return (0);
-	ft_strlcpy(result, s1 + start, end - start + 1);
+	result = ft_substr(s1, start, end - start + 1);
 	return (result);
 }
