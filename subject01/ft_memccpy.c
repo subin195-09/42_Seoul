0ef91ac426284115306a1a37713cfdd519d95c2b
subin195-09 <k_subin@naver.com>
2020-10-03 16:20:39 +0900

    10/03

--- subject01/ft_memccpy.c
+++ subject01/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 17:27:03 by skim              #+#    #+#             */
-/*   Updated: 2020/10/03 15:07:09 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 16:20:09 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	unsigned char *s;
-	unsigned char *d;
+	unsigned char	*s;
+	unsigned char	*d;
+	size_t			i;
 
 	if (!dest && !src)
 		return (0);
 	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
-	while (n)
+	i = 0;
+	while (i < n)
 	{
-		if (*s == (unsigned char)c)
-			return ((void *)d + 1);
-		*d++ = *s++;
-		n--;
+		d[i] = s[i];
+		if (s[i] == (unsigned char)c)
+			return (&d[i] + 1);
+		i++;
 	}
 	return (0);
 }
