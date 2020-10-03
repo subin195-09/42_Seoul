7245df352af3f8f11798e0069152e203826fb0b1
subin195-09 <k_subin@naver.com>
2020-10-03 16:22:20 +0900

    10/03

--- test/libft/ft_memmove.c
+++ test/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:52:19 by skim              #+#    #+#             */
-/*   Updated: 2020/10/01 17:25:45 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 16:22:11 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	if (!dest && !src)
+		return (0);
 	if (dest <= src)
 	{
 		while (n--)
