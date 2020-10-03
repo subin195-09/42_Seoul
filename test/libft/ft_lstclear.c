ba54d330fe0f8855201c81a96d53489f0d00e429
subin195-09 <k_subin@naver.com>
2020-10-03 15:53:19 +0900

    10/03

--- test/libft/ft_lstclear.c
+++ test/libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 18:01:01 by skim              #+#    #+#             */
-/*   Updated: 2020/10/03 15:05:05 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/03 15:52:44 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 		del((*lst)->content);
 		curr = *lst;
 		*lst = curr->next;
-		free(*lst);
+		free(curr);
 	}
 	*lst = 0;
 }
