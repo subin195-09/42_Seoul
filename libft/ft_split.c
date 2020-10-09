fc62ab108bd9fec51fd1759db24893880c581059
subin195-09 <k_subin@naver.com>
2020-10-09 12:47:49 +0900

    10/09

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 16:07:56 by skim              #+#    #+#             */
-/*   Updated: 2020/10/09 12:43:42 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/09 12:47:41 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ static int		make_split(char **result, const char *s, char c)
 				i++;
 			if (!(result[row] = malloc(i - start + 1)))
 				return (0);
-			ft_strlcpy(result[row++], &s[start], i - start + 1);
+			ft_strlcpy(result[row], &s[start], i - start + 1);
+			row++;
 		}
 		else
 			i++;
