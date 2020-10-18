787850b767c8f112eb575763d61a2c569825389f
subin195-09 <k_subin@naver.com>
2020-10-18 20:04:40 +0900

    10/18

--- ft_printf/hex.c
+++ ft_printf/hex.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_format_write.c                                  :+:      :+:    :+:   */
+/*   hex.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/16 18:15:35 by skim              #+#    #+#             */
-/*   Updated: 2020/10/16 21:42:31 by skim             ###   ########.fr       */
+/*   Updated: 2020/10/18 20:04:22 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,43 +33,4 @@ void	change_hex(const char **format, char *hex, int temp)
 		hex_table[i] = 'a' + add + (i - 10);
 		i++;
 	}
-}
-
-void	write_int(const char **format, va_list var, s_info info)
-{
-	int		temp;
-	int		count;
-	char	*hex;
-	char	*temp_result;
-
-	temp = va_arg(var, int);
-	change_hex(format, hex, temp);
-	if (**format == 'd' || **format == 'i')
-		count = count_num(temp);
-	else
-		count = ft_strlen(hex);
-	if (info.width > count && info.flag_in)
-	{
-		while (info.width > count)
-		{
-			write(1, info.flag_in, 1);
-			info.width--;
-		}
-	}
-	if (**format == 'd' || **format == 'i')
-		ft_putnbr_fd(temp, 1);
-	else
-		ft_putstr_fd(hex, 1);
-	(*format)++;
-}
-
-int		format_write(const char **format, va_list var, s_info info)
-{
-	if (**format == 'd' || **format == 'i' ||
-			**format == 'x' || **format == 'X')
-	{
-		ft_putnbr_fd(va_arg(var, int), 1);
-		(*format)++;
-	}
-	return (0);
 }
