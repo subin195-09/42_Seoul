9e78623fa983e43b056648b3b2b3d5ede745806c
subin195-09 <k_subin@naver.com>
2020-11-09 16:15:59 +0900

    11/09

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -6,15 +6,31 @@
 int main(void)
 {
 	setlocale(LC_ALL, "");
-	char	*test = "%#8.5x\n";
+	char	*test = "%*.*d\n";
 	int		byte = 0;
 	printf("%s\n", test);
 	printf("*result of  printf*\n");
-	byte = printf(test, 34);
+	byte = printf(test, -4, -3, 8);
 	printf("bytes : %d\n", byte);
 	printf("=======================\n");
 	printf("*result of ft_printf*\n");
-	byte = ft_printf(test, 34);
+	byte = ft_printf(test, -4, -3, 8);
+	byte = ft_printf("%*.*i", 4, -3, 8);
+	byte = ft_printf(test, -4, -3, 8);
+	byte = ft_printf("%*.*X", -4, -3, 8);
+	byte = ft_printf("%*.*lld", -4, -3, 8);
+	byte = ft_printf(test, -4, -3, 8);
+	byte = ft_printf(test, -4, -3, 8);
+	byte = ft_printf("%s", -4, -3, "kdjkfffff");
+	byte = ft_printf(test, -4, -3, 8);
+
+
 	printf("bytes : %d\n", byte);
+
+	while(1)
+	{
+		;
+	}
+
 }
 //wchar_t*
