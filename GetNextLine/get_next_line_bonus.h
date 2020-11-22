034a314bae3446efd7166a6e9c88f92c777923d2
subin195-09 <k_subin@naver.com>
2020-11-22 16:01:53 +0900

    11/22

---
+++ GetNextLine/get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/22 16:00:00 by skim              #+#    #+#             */
+/*   Updated: 2020/11/22 16:00:11 by skim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
+
+int		get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strndup(const char *src, int n);
+char	*ft_strjoin(const char *s1, const char *s2);
+
+#endif
