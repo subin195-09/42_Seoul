082ca93719b6b1ab65020329f7f8038642f19e91
subin195-09 <k_subin@naver.com>
2020-12-01 23:34:46 +0900

    12/01 hashtag 수정

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	setlocale(LC_ALL, "");
-	char	*test = "-->|%#*.*lx|<--\n";
+	char	*test = "-->|%#*.hx|<--\n";
 	int		byte = 0;
 
 	printf("%s\n", test);
 	printf("*result of  printf*\n");
-	byte = printf(test, 4, 3, 1);
+	byte = printf(test, 4, 1);
 	printf("bytes : %d\n", byte);
 	printf("=======================\n");
 	printf("*result of ft_printf*\n");
-	byte = ft_printf(test, 4, 3, 1);
+	byte = ft_printf(test, 4, 1);
 	printf("bytes : %d\n", byte);
 }
