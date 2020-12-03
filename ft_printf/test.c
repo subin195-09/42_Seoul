6ce76de48415015c7db665a1693e7ca693f1dc00
subin195-09 <k_subin@naver.com>
2020-12-03 22:21:29 +0900

    exam 준비

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	setlocale(LC_ALL, "");
-	char	*test = "-->|%-16.p|<--\n";
+	char	*test = "-->|%.0f|<--\n";
 	int		byte = 0;
 
 	printf("%s\n", test);
 	printf("*result of  printf*\n");
-	byte = printf(test, 0);
+	byte = printf(test, 3.512);
 	printf("bytes : %d\n", byte);
 	printf("=======================\n");
 	printf("*result of ft_printf*\n");
-	byte = ft_printf(test, 0);
+	byte = ft_printf(test, 3.512);
 	printf("bytes : %d\n", byte);
 }
