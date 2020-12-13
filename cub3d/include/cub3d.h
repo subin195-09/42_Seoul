d36a4bc1e5cb385ed3f423844b6245ebb29ac709
subin195-09 <k_subin@naver.com>
2020-12-13 20:07:51 +0900

    12/13 cub3d

--- cub3d/include/cub3d.h
+++ cub3d/include/cub3d.h
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   start.c                                            :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/13 03:00:33 by skim              #+#    #+#             */
-/*   Updated: 2020/12/13 15:20:03 by skim             ###   ########.fr       */
+/*   Created: 2020/12/13 15:17:31 by skim              #+#    #+#             */
+/*   Updated: 2020/12/13 15:19:48 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
-#include "cub3d.h"
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int press_esc_key(int key, void *param)
-{
-	if (key == 53 && param)
-	{
-		printf("STOP\n");
-		exit(0);
-	}
-	return (0);
-}
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(void)
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "창 띄워보기");
-	mlx_key_hook(win_ptr, press_esc_key, win_ptr);
-	mlx_loop(mlx_ptr);
-}
+#endif
