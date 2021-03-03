0d51e72f1552a6d25625ac11580ae68e03150b40
subin195-09 <k_subin@naver.com>
2021-03-03 18:26:53 +0900

    중간저장

--- cub3d/srcs/main_loop/casting_wall.c
+++ cub3d/srcs/main_loop/casting_wall.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 20:18:19 by skim              #+#    #+#             */
-/*   Updated: 2021/03/02 23:29:42 by skim             ###   ########.fr       */
+/*   Updated: 2021/03/03 18:05:08 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,6 +100,11 @@ void	wall_text(t_set *set, t_wcast *w, int x)
 		t.t_y = (int)t.t_pos & (TEXTHEIGHT - 1);
 		t.t_pos += step;
 		t.color = set->info.texture[t.t_num][TEXTHEIGHT * t.t_y + t.t_x];
+		for(int a = 0; a < set->up; a++)
+		{
+			t.color -= 0X010101;
+			t.color = t.color < 0 ? 0 : t.color;
+		}
 		re_y = y + set->jump + (set->up * 2);
 		re_y = make_re_y(set, re_y);
 		set->img.data[re_y * set->minfo.s_width + x] = t.color;
