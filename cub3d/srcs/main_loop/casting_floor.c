e839f75787c7c7801020d6378ead429f27529f64
skim <k_subin@naver.com>
2021-03-04 16:44:04 +0900

    생뚱

--- cub3d/srcs/main_loop/casting_floor.c
+++ cub3d/srcs/main_loop/casting_floor.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 21:55:54 by skim              #+#    #+#             */
-/*   Updated: 2021/03/03 19:17:04 by skim             ###   ########.fr       */
+/*   Updated: 2021/03/04 16:43:23 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,11 @@ void	floor_cast(t_set *set)
 		f.ray_dir_y0 = set->info.dir_y - set->info.plane_y;
 		f.ray_dir_x1 = set->info.dir_x + set->info.plane_x;
 		f.ray_dir_y1 = set->info.dir_y + set->info.plane_y;
-		make_re_y(set, y);
 		re_y = set->minfo.s_height / 2 + set->up * 2;
 		re_y = make_re_y(set, re_y);
 		f.p = y - set->minfo.s_height / 2 - set->up * 2;
 		f.p *= y < re_y ? -1 : 1;
-		f.pos_z = 0.5 * set->minfo.s_height - set->up * 2;
+		f.pos_z = 0.5 * set->minfo.s_height;
 		f.row_distance = f.pos_z / f.p;
 		f.floor_x = set->info.pos_x + f.row_distance * f.ray_dir_x0;
 		f.floor_y = set->info.pos_y + f.row_distance * f.ray_dir_y0;
--- cub3d/srcs/main_loop/resize_y.c
+++ cub3d/srcs/main_loop/resize_y.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 23:21:40 by skim              #+#    #+#             */
-/*   Updated: 2021/03/02 23:45:29 by skim             ###   ########.fr       */
+/*   Updated: 2021/03/04 16:31:29 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int		make_re_y(t_set *set, int y)
 {
-	if (y < 0)
+	if (y < 1)
 		return (0);
-	else if (y > set->minfo.s_height)
+	else if (y > set->minfo.s_height - 2)
 		return (set->minfo.s_height);
 	else
 		return (y);
