324d4499d77a705747618e8250c2d9937968d05f
skim <k_subin@naver.com>
2021-03-24 17:05:15 +0900

    libasm 정리

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -1,7 +1,5 @@
  #!/bin/bash
 
-#/bin/bash -C
-
 service mysql start
 service php7.3-fpm start
 nginx -g  "daemon off;"
--- libasm/ft_sum.s
+++
@@ -1,7 +0,0 @@
-segment	.text
-	global	_ft_sum
-
-_ft_sum:
-	add rdi, rsi
-	mov rax, rdi
-	ret
