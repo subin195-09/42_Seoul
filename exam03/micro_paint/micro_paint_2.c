a5d1ae20beb2a1fdfa572fb177352f7dd7d756b2
skim <k_subin@naver.com>
2021-03-29 18:46:20 +0900

    test 준비

--- exam03/micro_paint/micro_paint_2.c
+++ exam03/micro_paint/micro_paint_2.c
@@ -12,14 +12,15 @@ typedef struct	s_rect
 	char	type;
 	float	x;
 	float	y;
-	float	r_width;
-	float	r_height;
+	float	width;
+	float	height;
 	char	r_char;
 }				t_rect;
 
 int		ft_putstr(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		write(1, &s[i], 1);
@@ -28,39 +29,38 @@ int		ft_putstr(char *s)
 	return (1);
 }
 
-void	draw_rect(t_rect r, char **img, int x, int y)
+void	draw_rect(t_rect r, char **image, int x, int y)
 {
-	if ((float)x >= r.x && (float)x <= r.x + r.r_width &&\
-		(float)y >= r.y && (float)y <= r.y + r.r_height)
-			(*img)[y * r.b_width + x] = r.r_char;
+	if ((float)x >= r.x && (float)x <= r.x + r.width && (float)y >= r.y && (float)y <= r.y + r.height)
+		(*image)[y * r.b_width + x] = r.r_char;
 }
 
-int 	main(int ac, char *av[])
+int		main(int ac, char *av[])
 {
+	FILE	*file;
+	char	*image;
+	int		read;
 	int		x;
 	int		y;
-	char	*image;
-	FILE	*file;
-	int		read;
 	float	f_one = 1.0000000;
 	t_rect	r;
 
 	if (ac != 2)
 		return (ft_putstr("Error: argument\n"));
 	if (!(file = fopen(av[1], "r")))
-		return (ft_putstr("Error: Operation file corrupted\n"));
+		return(ft_putstr("Error: Operation file corrupted\n"));
 	read = fscanf(file, "%d %d %c\n", &r.b_width, &r.b_height, &r.b_char);
 	if (read != 3)
 		return (ft_putstr("Error: Operation file corrupted\n"));
-	if (!(r.b_width > 0 && r.b_width <= 300 && r.b_height > 0 && r.b_height <= 300))
+	if (!(r.b_width > 0 && r.b_width <= 300 & r.b_height > 0 && r.b_height <= 300))
 		return (ft_putstr("Error: Operation file corrupted\n"));
 
 	image = (char *)malloc(sizeof(char) * (r.b_width * r.b_height));
 	memset(image, r.b_char, r.b_width * r.b_height);
-	read = fscanf(file, "%c %f %f %f %f %c\n", &r.type, &r.x, &r.y, &r.r_width, &r.r_height, &r.r_char);
+	read = fscanf(file, "%c %f %f %f %f %c\n", &r.type, &r.x, &r.y, &r.width, &r.height, &r.r_char);
 	while (read == 6)
 	{
-		if (!((r.type == 'r' || r.type == 'R') && r.r_width > 0 && r.r_height > 0))
+		if (!((r.type == 'r' || r.type == 'R') && (r.width > 0 && r.height > 0)))
 		{
 			free(image);
 			return (ft_putstr("Error: Operation file corrupted\n"));
@@ -75,15 +75,15 @@ int 	main(int ac, char *av[])
 					draw_rect(r, &image, x, y);
 				else if (r.type == 'r')
 				{
-					if (((float)x - r.x < f_one || (r.x + r.r_width) - (float)x < f_one) ||
-							((float)y - r.y < f_one || (r.y + r.r_height) - (float)y < f_one))
+					if ((float)x - r.x < f_one || (r.x + r.width) - (float)x < f_one || \
+							(float)y - r.y < f_one || (r.y + r.height) - (float)y < f_one)
 						draw_rect(r, &image, x, y);
 				}
 				x++;
 			}
 			y++;
 		}
-		read = fscanf(file, "%c %f %f %f %f %c\n", &r.type, &r.x, &r.y, &r.r_width, &r.r_height, &r.r_char);
+		read = fscanf(file, "%c %f %F %f %f %c\n", &r.type, &r.x, &r.y, &r.width, &r.height, &r.r_char);
 	}
 	if (read != -1)
 	{
--- exam03/mini_paint/mini_paint_2.c
+++ exam03/mini_paint/mini_paint_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <math.h>
 
 typedef struct	s_cir
@@ -9,30 +9,38 @@ typedef struct	s_cir
 	int		b_width;
 	int		b_height;
 	char	b_char;
+	
 	char	type;
-	float	cen_x;
-	float	cen_y;
-	float	r;
-	char	c_char;
+	float	center_x;
+	float	center_y;
+	float	radius;
+	char	circle_c;
 }				t_cir;
 
 int		ft_putstr(char *s)
 {
-	int i = 0;
+	int	i = 0;
 	while (s[i])
-		write(1, &s[i++], 1);
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
 	return (1);
+}
+
+void	draw_circle(t_cir c, char **image, int x, int y, float distance)
+{
+	if (distance <= c.radius)
+		(*image)[y * c.b_width + x] = c.circle_c;
 }
 
 int		main(int ac, char *av[])
 {
-	int		x;
-	int		y;
-	float	distance;
-	float	f_one = 1.0000000;
 	FILE	*file;
 	int		read;
 	char	*image;
+	int		x;
+	int		y;
 	t_cir	c;
 
 	if (ac != 2)
@@ -44,13 +52,13 @@ int		main(int ac, char *av[])
 		return (ft_putstr("Error: Operation file corrupted\n"));
 	if (!(c.b_width > 0 && c.b_width <= 300 && c.b_height > 0 && c.b_height <= 300))
 		return (ft_putstr("Error: Operation file corrupted\n"));
-	
+
 	image = (char *)malloc(sizeof(char) * (c.b_width * c.b_height));
 	memset(image, c.b_char, c.b_width * c.b_height);
-	read = fscanf(file, "%c %f %f %f %c\n", &c.type, &c.cen_x, &c.cen_y, &c.r, &c.c_char);
+	read = fscanf(file, "%c %f %f %f %c\n", &c.type, &c.center_x, &c.center_y, &c.radius, &c.circle_c);
 	while (read == 5)
 	{
-		if (!((c.type == 'c' || c.type == 'C') && c.r > 0))
+		if (!((c.type == 'c' || c.type == 'C') && (c.radius > 0)))
 		{
 			free(image);
 			return (ft_putstr("Error: Operation file corrupted\n"));
@@ -61,25 +69,19 @@ int		main(int ac, char *av[])
 			x = 0;
 			while (x < c.b_width)
 			{
-				distance = sqrtf(powf(c.cen_x - (float)x, 2.) + powf(c.cen_y - (float)y, 2.));
+				float distance = sqrtf(powf((float)x - c.center_x, 2.) + powf((float)y - c.center_y, 2.));
 				if (c.type == 'C')
-				{
-					if (distance <= c.r)
-						image[y * c.b_width + x] = c.c_char;
-				}
+					draw_circle(c, &image, x, y, distance);
 				else if (c.type == 'c')
 				{
-					if (c.r - distance < f_one)
-					{
-						if (distance <= c.r)
-							image[y * c.b_width + x] = c.c_char;
-					}
+					if (c.radius - distance < 1.0000000)
+						draw_circle(c, &image, x, y, distance);
 				}
 				x++;
 			}
 			y++;
 		}
-		read = fscanf(file, "%c %f %f %f %c\n", &c.type, &c.cen_x, &c.cen_y, &c.r, &c.c_char);
+		read = fscanf(file, "%c %f %f %f %c\n", &c.type, &c.center_x, &c.center_y, &c.radius, &c.circle_c);
 	}
 	if (read != -1)
 	{
@@ -96,4 +98,4 @@ int		main(int ac, char *av[])
 	free(image);
 	fclose(file);
 	return (0);
-}	
+}
