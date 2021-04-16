84e1c9681fed221b83093cbd26878242c42233bc
subin195-09 <k_subin@naver.com>
2021-04-16 12:10:48 +0900

    update

--- minishell/test3_run.c
+++ minishell/test3_run.c
@@ -6,7 +6,7 @@
 /*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 18:19:42 by skim              #+#    #+#             */
-/*   Updated: 2021/04/15 22:43:30 by skim             ###   ########.fr       */
+/*   Updated: 2021/04/16 12:10:35 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,15 +88,18 @@ void	get_type(t_list *cmd)
 
 	// redirect인 경우 > 이후의 file open 후 얻은 descriptor를 fd[SIDE_IN]에 지정시켜준다.
 	char	**pipe_cmds;
-	int		i;
+	char	**temp_arg;
 
 	pipe_cmds = make_tok(cmd->arg[0], "|");
+	// 파이프 단위로 나뉜 것들을 일단 sibling 으로 연결
 	list_push(cmd, pipe_cmds);
-	i = -1;
-	while (cmds[++i])
+	while (cmd)
 	{
-		if (strstr(cmds[i], ">>"))
-
+		// make_tok에 쌍 따움표, 따움표도 같이 처리 할 수 있도록 추가
+		temp_arg = make_tok(cmd->arg[0], " ");
+		// 다이렉트 부분을 치환시켜서 open 시켜준 후 fd[SIDE_IN]과 연결
+		find_direct(&temp_arg, cmd);
+		cmd = cmd->sibing;
 	}
 }
 
@@ -112,7 +115,7 @@ int		normal_run(t_list cmd, char **en, char *av)
 	return (rt);
 }
 
-int		special_run(t_list cmd, char **en, char *av)
+int		pipe_run(t_list cmd, char **en, char *av)
 {
 	int	error_check;
 
@@ -122,13 +125,24 @@ int		special_run(t_list cmd, char **en, char *av)
 	error_check = dup2(cmd->pipes[SIND_IN], STDOUT);
 	if (error_check < 0)
 		return (EXIT_FAILURE);
-	error_check = dup2(cmd->prev->pipes[SIDE_OUT], STDIN);
-	if (error_check < 0)
+	normal_run(cmd, en, av);
+	// close 작업해주기
+	close(cmd->pipes[SIDE_IN]);
+}
+
+int		prev_pipe_run(t_list cmd, char **en, char *av)
+{
+	int	error_check;
+
+	error_check = pipe(cmd->pipes));
+	if (error_check)
 		return (EXIT_FAILURE);
+	error_check = dup2(cmd->prev->pipes[SIDE_OUT], STDIN);
 	if (error_check < 0)
 		return (EXIT_FAILURE);
 	normal_run(cmd, en, av);
 	// close 작업해주기
+	close(cmd->prev->pipes[SIDE_OUT]);
 }
 
 // 실행 단위로 분리된 t_list
@@ -136,14 +150,16 @@ int		run(t_list cmd, char **en, char *av)
 {
 	int		rt;
 
-	// pipe, >, >> 별로 type을 지정하고 sibling 관계로 연결을 시킨다.
+	// 바로 실행할 수 있도록 만들어준다.
 	get_type(cmd);
 	while (cmd->sibling)
 	{
-		if (cmd->type == TYPE_END)
+		if (cmd->type == TYPE_PIPE)
+			rt = pipe_run(cmd, en, av);
+		else if (cmd->prev && cmd->prev-type == TYPE_PIPE)
+			rt = prev_pipe_run(cmd, en, av);
+		else
 			rt = normal_run(cmd, en, av);
-		else // type이 PIPE이거나, REDIRECT일 경우
-			rt = special_run(cmd, en, av);
 		cmd = cmd->sibling;
 	}
 	return (rt);
