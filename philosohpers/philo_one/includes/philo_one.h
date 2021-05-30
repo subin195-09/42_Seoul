351be9a5173ae7a25a6ab3b87b7c95e4e3b6c440
Soobin Kim <skim@c4r1s6.42seoul.kr>
2021-05-30 17:59:01 +0900

    21/05/30 philosopher start

---
+++ philosohpers/philo_one/includes/philo_one.h
@@ -0,0 +1,16 @@
+#ifndef PHILO_ONE_H
+# define PHILO_ONE_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct  t_info{
+    int     num_of_philo;
+    int     time_to_die;
+    int     time_to_eat;
+    int     time_to_sleep;
+    int     time_must_eat;
+}               s_info;
+
+#endif
---
+++ philosohpers/philo_one/srcs/main.c
@@ -0,0 +1,86 @@
+#include "philo_one.h"
+
+int		ft_atoi(const char *str)
+{
+	int			i;
+	long long	num;
+	int			sign;
+
+	i = 0;
+	num = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		num = num * 10 + str[i++] - '0';
+	return ((int)(sign * num));
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int     ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+		return (-1);
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
+    if (fd == 2)
+        return (-1);
+    else
+        return (1);
+}
+
+
+void    get_info(s_info *info, int ac, char *av[])
+{
+    info->num_of_philo = ft_atoi(av[1]);
+    info->time_to_die = ft_atoi(av[2]);
+    info->time_to_eat = ft_atoi(av[3]);
+    info->time_to_sleep = ft_atoi(av[4]);
+    if (ac == 5)
+        info->time_must_eat = 1;
+    else
+        info->time_must_eat = ft_atoi(av[5]);
+}
+
+int     check_info(s_info *info)
+{
+    if (info->num_of_philo < 2)
+       return (ft_putendl_fd("Error : num of Philoshper", 2));
+    else if (info->time_to_die < 60)
+        return (ft_putendl_fd("Error : time to Die", 2));
+    else if (info->time_to_eat < 20)
+        return (ft_putendl_fd("Error : time to Eat", 2));
+    else if (info->time_to_sleep < 60)
+        return (ft_putendl_fd("Error : time to Sleep", 2));
+    else if (info->time_must_eat < 0)
+        return (ft_putendl_fd("Error : time must Die", 2));
+    return (1);
+}
+
+int     main(int ac, char *av[])
+{
+    s_info  info;
+
+    memset(&info, 0, sizeof(info));
+    if (ac != 5 && ac != 6)
+        return (ft_putendl_fd("Error : arg", 2));
+    get_info(&info, ac, av);
+    if (check_info(&info) < 0)
+        return (-1);
+    return (0);
+}
