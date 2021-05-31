0ca564cdfff801a0b57100875d1990f65c1f2dfb
Soobin Kim <skim@c1r8s7.42seoul.kr>
2021-06-01 06:57:12 +0900

    06/01 philosopher start

--- philosohpers/philo_one/includes/philo_one.h
+++ philosohpers/philo_one/includes/philo_one.h
@@ -6,36 +6,44 @@
 /*   By: skim <skim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 22:59:46 by skim              #+#    #+#             */
-/*   Updated: 2021/05/31 23:29:24 by skim             ###   ########.fr       */
+/*   Updated: 2021/06/01 06:40:32 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_ONE_H
 # define PHILO_ONE_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <pthread.h>
+# include <sys/time.h>
 
-typedef struct  t_philo{
+typedef struct  s_philo{
     pthread_t       p_th;
-    pthread_mutex_t p_mu;
+    pthread_mutex_t p_mu_eat;
+    struct s_info   *info;
+    int             p_num;
+    int             count_eat;
     int             l_fork;
     int             r_fork;
-    int             count_eat;
-    int             p_num;
-}               s_philo;
+}               t_philo;
 
-typedef struct  t_info{
+typedef struct  s_info{
     int             num_of_philo;
     int             time_to_die;
     int             time_to_eat;
     int             time_to_sleep;
     int             num_must_eat;
-    struct t_philo  ph;
-}               s_info;
+    int             stop;
+    int             done_eat;
+    struct s_philo  *ph;
+    pthread_mutex_t *fork;
+    pthread_mutex_t text;
+}               t_info;
 
-int     philo_main(s_info *info);
+int     philo_main(t_info *info);
 
 int     ft_putendl_fd(char *s, int fd);
 size_t	ft_strlen(const char *str);
--- philosohpers/philo_one/srcs/philo_main.c
+++ philosohpers/philo_one/srcs/philo_main.c
@@ -6,44 +6,115 @@
 /*   By: skim <skim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 21:53:55 by skim              #+#    #+#             */
-/*   Updated: 2021/05/31 23:30:54 by skim             ###   ########.fr       */
+/*   Updated: 2021/06/01 06:52:22 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-void    init_philo(s_philo *ph)
+void    mutext_print(t_philo *ph, char *s, int ph_num)
 {
-    ph->count_eat = 0;
+	pthread_mutex_lock(&ph->info->text);
+	printf("%d : %s", ph_num, s);
+	pthread_mutex_unlock(&ph->info->text);
+}
+
+void    pick_up_fork(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->info->fork[ph->p_num]);
+	mutext_print(ph, "pick up fork\n", ph->p_num);
+	pthread_mutex_lock(&ph->info->fork[(ph->p_num + 1) % ph->info->num_of_philo]);
+	mutext_print(ph, "pick up fork\n", ph->p_num);
+}
+
+void    eat(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->p_mu_eat);
+	mutext_print(ph, "eating\n", ph->p_num);
+	usleep(ph->info->time_to_eat * 1000);
+	ph->count_eat++;
+	pthread_mutex_unlock(&ph->p_mu_eat);
+}
+
+void    return_fork(t_philo *ph)
+{
+	pthread_mutex_unlock(&ph->info->fork[ph->p_num]);
+	mutext_print(ph, "return fork\n", ph->p_num);
+	pthread_mutex_unlock(&ph->info->fork[(ph->p_num + 1) % ph->info->num_of_philo]);
+	mutext_print(ph, "return fork\n", ph->p_num);
+}
+
+void    sleeping(t_philo *ph)
+{
+	mutext_print(ph, "sleeping\n", ph->p_num);
+	usleep(ph->info->time_to_sleep * 1000);
+}
+
+int     check_eat_count(t_philo *ph)
+{
+	if (ph->count_eat == ph->info->num_must_eat)
+		ph->info->done_eat++;
+	if (ph->info->done_eat == ph->info->num_of_philo)
+	{
+		mutext_print(ph, "done eat all\n", ph->p_num);
+		ph->info->stop = 1;
+		return (-1);
+	}
+	return (1);
 }
 
 void    *ph_start(void  *arg)
 {
-    s_info *ph;
-    int     i;
+	t_philo *philo;
+	int     i;
 
-    ph = (s_info *)arg;
-    i = -1;
-    while (++i < )
+	philo = (t_philo *)arg;
+	i = -1;
+	while (++i < philo->info->num_must_eat)
+	{
+		pick_up_fork(philo);
+		eat(philo);
+		return_fork(philo);
+		if (check_eat_count(philo) < 0)
+			break ;
+		sleeping(philo);
+		mutext_print(philo, "thinking\n", philo->p_num);
+	}
+	pthread_mutex_unlock(&philo->info->fork[philo->p_num]);
+	pthread_mutex_unlock(&philo->info->fork[(philo->p_num + 1) % philo->info->num_of_philo]);
+	pthread_mutex_unlock(&philo->info->text);
+	return (0);
 }
 
-int     philo_main(s_info *info)
+void    *check_die(void *arg)
 {
-    s_philo ph[info->num_of_philo];
-    int     i;
+	t_philo *philo;
 
-    init_philo(&ph);
-    i = -1;
-    while (++i < info->num_of_philo)
-    {
-        info->ph = ph[i];
-        if (pthread_create(&(ph[i].p_th), NULL, ph_start, info))
-            return (ft_putendl_fd("Error : create thread", 2));
-    }
-    i = -1;
-    while (++i < info->num_of_philo)
-        pthread_join(ph[i], NULL);
-    i = -1;
-    while (++i < info->num_of_philo)
-        pthread_mutex_destroy(&(ph.p_mu));
+	philo = (t_philo *)arg;
+	usleep(1000 * 1000);
+	return (0);
+}
+
+int     philo_main(t_info *info)
+{
+	pthread_t   thread;
+	int         i;
+
+	i = -1;
+	while (++i < info->num_of_philo)
+	{
+		if (pthread_create(&thread, NULL, ph_start, (void *)&(info->ph[i])))
+			return (ft_putendl_fd("Error : ph_start", 2));
+		pthread_detach(thread);
+		if (pthread_create(&(info->ph[i].p_th), NULL, check_die, (void *)&(info->ph[i])))
+			return (ft_putendl_fd("Error : ph_start", 2));
+		usleep(1000);
+	}
+	i = -1;
+	while (++i < info->num_of_philo)
+		pthread_join(info->ph[i].p_th, NULL);
+	i = -1;
+	while (++i < info->num_of_philo)
+		pthread_mutex_destroy(&(info->ph[i].p_mu_eat));
+	return (0);
 }
