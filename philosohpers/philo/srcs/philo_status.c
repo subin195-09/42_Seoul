919e91102cafc4c97ea044eb3370d7d286066ed3
Soobin Kim <skim@c3r5s4.42seoul.kr>
2021-06-08 21:54:56 +0900

    06/08 philosopher start

---
+++ philosohpers/philo/srcs/philo_status.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_status.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skim <skim@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 21:40:45 by skim              #+#    #+#             */
+/*   Updated: 2021/06/08 21:01:58 by skim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	pick_up_fork(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->info->fork[ph->p_num]);
+	mutext_print(ph, "pick up fork\n", ph->p_num, 0);
+	pthread_mutex_lock(&ph->info->fork[(ph->p_num + 1)\
+	% ph->info->num_of_philo]);
+	mutext_print(ph, "pick up fork\n", ph->p_num, 0);
+}
+
+void	eat(t_philo *ph)
+{
+	pthread_mutex_lock(&ph->p_mu_eat);
+	mutext_print(ph, "eating\n", ph->p_num, 0);
+	usleep(ph->info->time_to_eat * 1000);
+	ph->count_eat++;
+	pthread_mutex_unlock(&ph->p_mu_eat);
+}
+
+void	return_fork(t_philo *ph)
+{
+	pthread_mutex_unlock(&ph->info->fork[ph->p_num]);
+	mutext_print(ph, "return fork\n", ph->p_num, 0);
+	pthread_mutex_unlock(&ph->info->fork[(ph->p_num + 1)\
+	% ph->info->num_of_philo]);
+	mutext_print(ph, "return fork\n", ph->p_num, 0);
+}
+
+void	sleeping(t_philo *ph)
+{
+	mutext_print(ph, "sleeping\n", ph->p_num, 0);
+	usleep(ph->info->time_to_sleep * 1000);
+}
