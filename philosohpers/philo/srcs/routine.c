dc55a4f4dec870a29cee42ccc68fa9c8a6c0db50
Soobin Kim <skim@c4r2s8.42seoul.kr>
2021-06-15 22:28:31 +0900

    06/15 philosopher start

--- philosohpers/philo/srcs/routine.c
+++ philosohpers/philo/srcs/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skim <skim@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: skim <skim@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 20:58:06 by skim              #+#    #+#             */
-/*   Updated: 2021/06/15 21:32:24 by skim             ###   ########.fr       */
+/*   Updated: 2021/06/15 22:27:49 by skim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,26 @@
 
 void	get_fork(t_philo *philo)
 {
-	if (philo->philo_num)
-	pthread_mutex_lock(&philo->main->fork[philo->l_fork]);
-	philo->main->fork_flag[philo->l_fork] = 1;
-	philo_print(philo, "pick_up fork");
-	pthread_mutex_lock(&philo->main->fork[philo->r_fork]);
-	philo->main->fork_flag[philo->r_fork] = 1;
-	philo_print(philo, "pick_up fork");
+	if (philo->philo_num != philo->main->num_of_philo - 1)\
+	{
+		pthread_mutex_lock(&philo->main->fork[philo->l_fork]);
+		philo_print(philo, "pick_up fork");
+		pthread_mutex_lock(&philo->main->fork[philo->r_fork]);
+		philo_print(philo, "pick_up fork");
+	}
+	else
+	{
+		pthread_mutex_lock(&philo->main->fork[philo->r_fork]);
+		philo_print(philo, "pick_up fork");
+		pthread_mutex_lock(&philo->main->fork[philo->l_fork]);
+		philo_print(philo, "pick_up fork");
+	}
 }
+
+void	eat(t_philo *philo)
+{
+	philo_print(philo, "eating");
+	usleep(philo->main->arg_info.time_to_eat * 1000);
+	philo->count_eat++;
+}
+
