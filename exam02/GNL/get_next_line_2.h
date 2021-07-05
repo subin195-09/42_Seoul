ca5395c1702b9ad0bb092f587e4bbc05eb858598
Soobin Kim <skim@c3r5s7.42seoul.kr>
2021-07-05 23:14:41 +0900

    sep exam

--- exam02/GNL/get_next_line_2.h
+++
@@ -1,8 +0,0 @@
-#ifndef GET_NEXT_LINE_2
-# define GET_NEXT_LINE_2
-
-# include <stdlib.h>
-# include <unistd.h>
-
-#endif
-
--- exam02/GNL/get_next_line_3.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_NEXT_LINE_3
-# define GET_NEXT_LINE_3
-
-#include <unistd.h>
-#include <stdlib.h>
-
-int get_next_line(char **line);
-
-#endif
